6157cb5c465f46448d0e5cc5389401ebaae65b12
gzstyp <444141300@qq.com>
2020-10-25 13:02:26 +0800

    添加了修改文件名方法

--- src/main/java/com/fwtai/Launch.java
+++ src/main/java/com/fwtai/Launch.java
@@ -13,7 +13,11 @@ public class Launch{
     if(args.length > 0){
       final String path = args[0];
       System.out.println(path);
-      bilibili(path);
+      replace(path);
+      if(args.length > 1){
+        final String exist = args[1];
+        delPrefix(path);
+      }
     }
   }
 
@@ -30,7 +34,7 @@ public class Launch{
             final String name = childFiles[i].getPath();
             final String fileName = name.replaceAll(expression,(target==null ? "" : target));
             new File(name).renameTo(new File(fileName));
-          } catch (Exception e) {}
+          } catch (Exception e) {return false;}
         }
       }
       return true;
@@ -39,7 +43,7 @@ public class Launch{
     }
   }
 
-  protected static boolean bilibili(final String pathDir){
+  protected static boolean replace(final String pathDir){
     final File dir = new File(pathDir);
     try {
       if(dir.isDirectory()){
@@ -53,7 +57,42 @@ public class Launch{
             final String fileName = name.replaceAll(expression,"");
             new File(name).renameTo(new File(fileName));
             dirRename(pathDir,"\\(\\)","");
-          } catch (Exception e){}
+          } catch (Exception e){return false;}
+        }
+      }
+      return true;
+    } catch (Exception e){
+      return false;
+    }
+  }
+
+  protected static boolean delPrefix(final String pathDir){
+    final File dir = new File(pathDir);
+    try {
+      if(dir.isDirectory()){
+        final File[] childFiles = dir.listFiles();
+        for(int i = 0; i < childFiles.length; i++){
+          final String name = childFiles[i].getPath();
+          final String separator = File.separator;
+          final int last = name.lastIndexOf(separator);
+          final String file_name = name.substring(last + 1);
+          final String file_dir = name.substring(0,last);
+          try {
+            final String less10 = file_name.substring(1,2);
+            if(less10.equals(".")){
+              final String sub = file_name.substring(2);
+              final String fileName = file_dir + separator + sub;
+              new File(name).renameTo(new File(fileName));
+            }
+            final String greater10 = file_name.substring(2,3);
+            if(greater10.equals(".")){
+              final String sub = file_name.substring(3);
+              final String fileName = file_dir + separator + sub;
+              new File(name).renameTo(new File(fileName));
+            }
+          } catch (Exception e){
+            return false;
+          }
         }
       }
       return true;
