8d07e7c6dc64f8511908a1aee174690d8f07b5d1
引路者 <444141300@qq.com>
2021-08-09 23:47:31 +0800

    重命名文件夹

--- src/main/java/com/fwtai/Launcher.java
+++ src/main/java/com/fwtai/Launcher.java
@@ -10,6 +10,7 @@ import java.io.File;
  * 示例:
  * "F:\bilibili\下载的目录" 1
  * "F:\bilibili\下载的目录" 2 "替换的字符串"
+ * 若需要删除以数字开头的则先执行 "F:\bilibili\下载的目录" 1,若执行完后还需要替换指定的字符串再执行 "F:\bilibili\下载的目录" 2 "被替换的字符串"
  * @作者 田应平
  * @版本 v1.0
  * @创建时间 2020/12/7 15:22
@@ -32,14 +33,12 @@ public class Launcher{
             case 1:// "F:\bilibili\xxx" 1
               delPrefix(path);
               break;
-            case 2:// "F:\bilibili\xxx" 2 "4-"
+            case 2:// "F:\bilibili\xxx" 2 "4-";
               if(args.length > 2){
                 final String expression = args[2];
                 if(expression != null && expression.length() >0)
                   delExpression(path,expression);
               }
-              break;
-            case 3:
               break;
             default:
               break;
@@ -48,116 +47,94 @@ public class Launcher{
     }
   }
 
-  protected static boolean dirRename(final String pathDir,final String expression,final String target){
+  protected static void dirRename(final String pathDir,final String expression,final String target){
     final File dir = new File(pathDir);
     if (expression == null) {
-      return false;
+      return;
     }
-    try {
-      if(dir.isDirectory()){
-        final File[] childFiles = dir.listFiles();
-        for(int i = 0; i < childFiles.length; i++){
-          try {
-            final String name = childFiles[i].getPath();
-            final String fileName = name.replaceAll(expression,(target==null ? "" : target));
-            new File(name).renameTo(new File(fileName));
-          } catch (Exception e) {return false;}
-        }
+    if(dir.isDirectory()){
+      final File[] childFiles = dir.listFiles();
+      for(int i = 0; i < childFiles.length; i++){
+        try {
+          final String name = childFiles[i].getPath();
+          final String fileName = name.replaceAll(expression,(target==null ? "" : target));
+          new File(name).renameTo(new File(fileName));
+        } catch (final Exception e){}
       }
-      return true;
-    } catch (Exception e){
-      return false;
     }
   }
 
-  protected static boolean replace(final String pathDir){
+  protected static void replace(final String pathDir){
     final File dir = new File(pathDir);
-    try {
-      if(dir.isDirectory()){
-        final File[] childFiles = dir.listFiles();
-        for(int i = 0; i < childFiles.length; i++){
-          final String name = childFiles[i].getPath();
-          int prefix = name.lastIndexOf("(A");
-          int suffix = name.lastIndexOf(").")+1;
-          try {
-            final String expression = name.substring(prefix,suffix);
-            final String fileName = name.replaceAll(expression,"");
-            new File(name).renameTo(new File(fileName));
-            dirRename(pathDir,"\\(\\)","");
-          } catch (Exception e){return false;}
-        }
+    if(dir.isDirectory()){
+      final File[] childFiles = dir.listFiles();
+      for(int i = 0; i < childFiles.length; i++){
+        final String name = childFiles[i].getPath();
+        int prefix = name.lastIndexOf("(A");
+        int suffix = name.lastIndexOf(").")+1;
+        try {
+          final String expression = name.substring(prefix,suffix);
+          final String fileName = name.replaceAll(expression,"").replaceAll("\\(","").replaceAll("\\)","");
+          new File(name).renameTo(new File(fileName));
+          dirRename(pathDir,"\\(\\)","");
+        } catch (final Exception e){}
       }
-      return true;
-    } catch (Exception e){
-      return false;
     }
   }
 
-  private static boolean delPrefix(final String pathDir){
+  private static void delPrefix(final String pathDir){
     final File dir = new File(pathDir);
-    try {
-      if(dir.isDirectory()){
-        final File[] childFiles = dir.listFiles();
-        for(int i = 0; i < childFiles.length; i++){
-          final String name = childFiles[i].getPath();
-          final String separator = File.separator;
-          final int last = name.lastIndexOf(separator);
-          final String file_name = name.substring(last + 1);
-          final String file_dir = name.substring(0,last);
-          try {
-            final String less10 = file_name.substring(1,2);
-            if(less10.equals(".")){
-              final String sub = file_name.substring(2);
-              final String fileName = file_dir + separator + sub;
-              new File(name).renameTo(new File(fileName));
-            }
-            final String greater10 = file_name.substring(2,3);
-            if(greater10.equals(".")){
-              final String sub = file_name.substring(3);
-              final String fileName = file_dir + separator + sub;
-              new File(name).renameTo(new File(fileName));
-            }
-            final String greater100 = file_name.substring(3,4);
-            if(greater100.equals(".")){
-              final String sub = file_name.substring(4);
-              final String fileName = file_dir + separator + sub;
-              new File(name).renameTo(new File(fileName));
-            }
-          } catch (Exception e){
-            return false;
-          }
-        }
-      }
-      return true;
-    } catch (Exception e){
-      return false;
-    }
-  }
-
-  private static boolean delExpression(final String pathDir,final String expression){
-    final File dir = new File(pathDir);
-    try {
-      if(dir.isDirectory()){
-        final File[] childFiles = dir.listFiles();
-        for(int i = 0; i < childFiles.length; i++){
-          final String name = childFiles[i].getPath();
-          final String separator = File.separator;
-          final int last = name.lastIndexOf(separator);
-          final String file_name = name.substring(last + 1);
-          final String file_dir = name.substring(0,last);
-          try {
-            final int length = expression.length();
-            final String sub = file_name.substring(length);
+    if(dir.isDirectory()){
+      final File[] childFiles = dir.listFiles();
+      for(int i = 0; i < childFiles.length; i++){
+        final String name = childFiles[i].getPath();
+        final String separator = File.separator;
+        final int last = name.lastIndexOf(separator);
+        final String file_name = name.substring(last + 1);
+        final String file_dir = name.substring(0,last);
+        try {
+          final String less10 = file_name.substring(1,2);
+          if(less10.equals(".")){
+            final String sub = file_name.substring(2);
             final String fileName = file_dir + separator + sub;
             new File(name).renameTo(new File(fileName));
-          } catch (Exception e){
-            return false;
           }
-        }
+          final String greater10 = file_name.substring(2,3);
+          if(greater10.equals(".")){
+            final String sub = file_name.substring(3);
+            final String fileName = file_dir + separator + sub;
+            new File(name).renameTo(new File(fileName));
+          }
+          final String greater100 = file_name.substring(3,4);
+          if(greater100.equals(".")){
+            final String sub = file_name.substring(4);
+            final String fileName = file_dir + separator + sub;
+            new File(name).renameTo(new File(fileName));
+          }
+        } catch (final Exception e){}
       }
-      return true;
-    } catch (Exception e){
-      return false;
+    }
+  }
+
+  private static void delExpression(final String pathDir,final String expression){
+    final File dir = new File(pathDir);
+    if(dir.isDirectory()){
+      final File[] childFiles = dir.listFiles();
+      for(int i = 0; i < childFiles.length; i++){
+        final String name = childFiles[i].getPath();
+        final String separator = File.separator;
+        final int last = name.lastIndexOf(separator);
+        final String file_name = name.substring(last + 1);
+        final String file_dir = name.substring(0,last);
+        try {
+          final int length = expression.length();
+          final int index = file_name.lastIndexOf(expression);
+          final String sub = file_name.substring(index,length+index);
+          final String file = file_name.replaceAll(sub,"");
+          final String fileName = file_dir + separator + file;
+          new File(name).renameTo(new File(fileName));
+        } catch (final Exception e){}
+      }
     }
   }
 }
